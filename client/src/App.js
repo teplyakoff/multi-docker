fcb76770375a3a3c1b54a3f0395d6f1a2ac5259d
Dmitry Teplyakov <dmitry.teplyakov@together.com>
2022-09-14 23:03:27 +0300

    my change

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ function App() {
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
+          <p1>Fib calculator</p1>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
